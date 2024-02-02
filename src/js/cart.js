fa68448f9f06bb7fed02725eaeac07b0eb41a04c
Ross Gardner <gar17014@byui.edu>
2024-02-01 17:01:52 -0700

    Completed Total in Cart trello card

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,21 @@ function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  // Check if there are items in the cart
+  if  (cartItems.length > 0) {
+    // Show the cart footer
+    document.querySelector('.cart-footer').classList.toggle('hide');
+
+    // Calculate total
+    var total = 0;
+    cartItems.forEach(function(item){
+      total += item.FinalPrice;
+    });
+    console.log(total);
+
+    // Insert total into HTML element
+    document.getElementById('totalAmount').innerText = '$' + total.toFixed(2);
+  }
 }
 
 function cartItemTemplate(item) {
