f6d15095829664230a4c69796969f7ea19a408d1
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-15 16:11:55 -0700

    Update styles and add product grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
 import { renderHeader, renderFooter } from './utils.mjs';
 import ProductList from './components/ProductList.svelte';
 
-new ProductList({
-  target: document.querySelector('.products'),
-  props: { category: 'tents' }
-});
-
 renderHeader();
 renderFooter();
--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig({
         product1: resolve(__dirname, 'src/product_pages/cedar-ridge-rimrock-2.html'),
         product2: resolve(__dirname, 'src/product_pages/marmot-ajax-3.html'),
         product3: resolve(__dirname, 'src/product_pages/northface-alpine-3.html'),
-        product4: resolve(__dirname, 'src/product_pages/northface-talus-4.html')
+        product4: resolve(__dirname, 'src/product_pages/northface-talus-4.html'),
+        productlist: resolve(__dirname, 'src/product-list/index.html')
       }
     }
   }
