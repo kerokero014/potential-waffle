f1f4ee46444d33157dd5955c5edb738f5453ad1d
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-15 19:05:15 -0700

    Refactor ProductSummary and productList components

--- src/js/components/ProductSummary.svelte
+++ src/js/components/ProductSummary.svelte
@@ -1,22 +1,24 @@
 <script>
-    export let product = {};
+  export let product = {};
 
-     // Calculate discount percentage
-  $: discountPercentage = Math.round(((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100);
+  // Calculate discount percentage
+  $: discountPercentage = Math.round(
+    ((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100
+  );
 </script>
-  
-  <section>
-    <a href="product_pages/index.html?product={product.Id}">
-      <img src={product.Image} alt="Image of {product.Name}" />
-      <h3 class="card__brand">{product.Brand.Name}</h3>
-      <h2 class="card__name">{product.NameWithoutBrand}</h2>
-      <!-- <p class="product-card__price">${product.FinalPrice}</p> -->
-      {#if product.FinalPrice < product.SuggestedRetailPrice}
-        <p class="discount">${product.FinalPrice}</p>
-        <p class="strikethrough">${product.SuggestedRetailPrice}</p>
-        <span class="discount-indicator">-{discountPercentage}%</span>
-      {:else}
-        <p>${product.SuggestedRetailPrice}</p>
-      {/if}
-    </a>
-  </section>
+
+<section>
+  <a href="/product_pages/index.html?product={product.Id}">
+    <img src={product.Images.PrimaryMedium} alt="Image of {product.Name}" />
+    <h3 class="card__brand">{product.Brand.Name}</h3>
+    <h2 class="card__name">{product.NameWithoutBrand}</h2>
+    <!-- <p class="product-card__price">${product.FinalPrice}</p> -->
+    {#if product.FinalPrice < product.SuggestedRetailPrice}
+      <p class="discount">${product.FinalPrice}</p>
+      <p class="strikethrough">${product.SuggestedRetailPrice}</p>
+      <span class="discount-indicator">-{discountPercentage}%</span>
+    {:else}
+      <p>${product.SuggestedRetailPrice}</p>
+    {/if}
+  </a>
+</section>
