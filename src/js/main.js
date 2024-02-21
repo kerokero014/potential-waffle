b6f0b6bb385140b604016ffcdc64e922a7775ff9
JoshWoods1 <woo19047@byui.edu>
2024-02-20 21:26:35 -0700

    alert card finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import BannerView from './components/BannerView.svelte';
 import { renderHeader, renderFooter } from './utils.mjs';
+import { renderAlerts } from './alert.js'; 
 
 const app = new BannerView({
   target: document.getElementById('app')
@@ -7,3 +8,7 @@ const app = new BannerView({
 
 renderHeader();
 renderFooter();
+
+// Render alerts
+const alertsContainer = document.getElementById('alerts');
+alertsContainer.appendChild(await renderAlerts()); 
